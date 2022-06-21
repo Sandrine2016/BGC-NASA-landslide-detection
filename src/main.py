030a237fefb4209f0a22faaf8365a95905f216b7
yiting9529 <90019050+yiting9529@users.noreply.github.com>
2022-06-20 17:41:43 -0700

    Updated result

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 import pandas as pd
-from src.location_predictions import location as loc , data_processing
-from src.time_normalizer import time_prediction as time
+from location_predictions import location as loc , data_processing
+from time_normalizer import time_prediction as time
+
 
 if __name__ == '__main__':
     original_data = pd.read_csv('../data/output/article_sample.tsv', sep='\t')
     clean_data = data_processing.prepare_date(original_data)
     loc_results = loc.predict(clean_data)
     time_results = time.get_final_result(clean_data, original_data)
-    data_processing.get_final_result(original_data, loc_results, time_results)
+    nasa_data = pd.read_csv('../data/nasa/nasa_global_landslide_catalog_point.csv')
+    data_processing.get_final_result(original_data, loc_results, time_results, nasa_data)
 
 
 
